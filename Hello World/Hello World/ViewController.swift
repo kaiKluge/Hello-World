53b0d99181a8a6cdf1089c7a5f90ddd24f9ae712
Kai Kluge <kluge@candy4you.de>
2020-07-04 15:10:17 +0200

    Build UI and created IBActions and IBOutlet.

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func helloSelected(_ sender: UIButton) {
+        
+    }
 
+    @IBAction func clearSelected(_ sender: UIButton) {
+    }
 
 }
 
